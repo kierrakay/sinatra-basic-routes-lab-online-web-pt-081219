38a3965a4ddf826ddfe7f97b97c7e00e3975c932
Kierra M <k.merriman32@hotmail.com>
2019-11-14 01:38:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+    "My name is Maddison!"
+  end
+  
+  get '/hometown' do 
+    "My hometown is Haymarket"
+  end
+  
+  get '/favorite-song' do 
+    "My favorite song is love"
+end
 end
